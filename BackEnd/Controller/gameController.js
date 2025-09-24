2d46642558a98c00a73205047dad360dd0668dbc
Hamad <hamadnabizada@gmail.com>
2025-09-24 11:04:31 -0700

    updated /api/game/daily GET to return data for a random game

--- BackEnd/Controller/gameController.js
+++ BackEnd/Controller/gameController.js
@@ -2,6 +2,61 @@
 
 //return the game of the day 
 export const getDailyGame = (req, res)=>{
-    res.status(200).send('getDailyGame route called')
+    const gameData =  getRandomGame()
+  res.status(200).send(gameData)
 }
 
+
+
+
+
+
+//Return a random game
+const getRandomGame = ()=>{
+    const data = {
+    "id": 2,
+    "first_release_date": 912384000,
+    "genres": [
+      {
+        "id": 13,
+        "name": "Simulator"
+      },
+      {
+        "id": 31,
+        "name": "Adventure"
+      }
+    ],
+    "involved_companies": [
+      {
+        "id": 7,
+        "company": {
+          "id": 4,
+          "name": "Eidos Interactive"
+        }
+      },
+      {
+        "id": 6,
+        "company": {
+          "id": 3,
+          "name": "Looking Glass Studios"
+        }
+      },
+      {
+        "id": 265431,
+        "company": {
+          "id": 1488,
+          "name": "Activision Value"
+        }
+      }
+    ],
+    "name": "Thief: The Dark Project",
+    "platforms": [
+      {
+        "id": 6,
+        "name": "PC (Microsoft Windows)"
+      }
+    ],
+    "rating": 86.63396922169207
+  }
+    return data
+}
