0e26846ce91d787f236848311d40b5e4217aa6c9
ParthD25 <pdave7848@gmail.com>
2025-09-29 17:43:11 -0700

    added use of db to generate random game for the day and handle case sensitivity

--- BackEnd/Controller/gameController.js
+++ BackEnd/Controller/gameController.js
@@ -1,7 +1,7 @@
 import {DB } from '../DB/connectDb.js'
 
 
-//return the game of the day 
+//return the game of the day randomly selected 
 export const getDailyGame = (req, res)=>{
     DB.get( 'SELECT * FROM games ORDER BY RANDOM() LIMIT 1', [], (err, row) => {
         if (err) {
@@ -17,19 +17,33 @@ export const getDailyGame = (req, res)=>{
 
 
         res.status(200).send(row)
-    })
+    } )
 }
+
+
 
 //return a game based on the title
 export const searchGameByTitle = (req, res) =>{
     const { title } = req.body
-    //check if game exists
-    const gameData = lookUpByTitle(title)
-    //return if exists
-        res.status(200).send(gameData)
-    //error handling
+
+    if (!title || title.trim() === '') {
+        return res.status(400).send({ error: 'Title is required ' })
+    }
+DB.get('SELECT * FROM games WHERE LOWER (name) = LOWER(?)', [title], (err, row) => {
+    // only find game titles with exact title lowercase of case sensitivity.
+    if (err) {
+        //handles error for database issues
+        console.error('Database error:', err)
+        return res.status(500).send({ error: 'Database error' });
+    }
+    if (!row) {
+        //if no games were found throw an error
+        return res.status(404).send({ error: ' Game not found' });
+    }
+    res.status(200).send(row);
 }
+);
 
 
 
-    
+}
