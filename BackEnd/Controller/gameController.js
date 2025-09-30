1d9b42158dc284a0827380e1c75ae3f28c77cf83
ParthD25 <pdave7848@gmail.com>
2025-09-29 17:30:27 -0700

    using db for controller for dailygames and delete old functions not in use

--- BackEnd/Controller/gameController.js
+++ BackEnd/Controller/gameController.js
@@ -1,8 +1,23 @@
+import {DB } from '../DB/connectDb.js'
+
 
 //return the game of the day 
 export const getDailyGame = (req, res)=>{
-    const gameData =  getRandomGame()
-  res.status(200).send(gameData)
+    DB.get( 'SELECT * FROM games ORDER BY RANDOM() LIMIT 1', [], (err, row) => {
+        if (err) {
+          //error handling for database issues
+            console.error('Database error:', err)
+              return res.status(500).send( { error: 'Database error' } )
+        }
+        if (!row) {
+          //if no game found throw error
+
+            return res.status(404).send( { error: 'No games found' } )
+        }
+
+
+        res.status(200).send(row)
+    })
 }
 
 //return a game based on the title
@@ -17,101 +32,4 @@ export const searchGameByTitle = (req, res) =>{
 
 
 
-
-//DB LookUps
-const lookUpByTitle = title =>{
-    const data = {
-    "id": 47,
-    "first_release_date": 1245715200,
-    "genres": [
-      {
-        "id": 25,
-        "name": "Hack and slash/Beat 'em up"
-      },
-      {
-        "id": 31,
-        "name": "Adventure"
-      }
-    ],
-    "involved_companies": [
-      {
-        "id": 924,
-        "company": {
-          "id": 115,
-          "name": "Codemasters"
-        }
-      },
-      {
-        "id": 923,
-        "company": {
-          "id": 163,
-          "name": "Climax Studios"
-        }
-      }
-    ],
-    "name": "Overlord: Dark Legend",
-    "platforms": [
-      {
-        "id": 5,
-        "name": "Wii"
-      }
-    ]
-  }
-  return data
-}
-
-
-
-
-//UTILITY FUNCTIONS
-
-
-//Return a random game
-const getRandomGame = ()=>{
-    const data = {
-    "id": 2,
-    "first_release_date": 912384000,
-    "genres": [
-      {
-        "id": 13,
-        "name": "Simulator"
-      },
-      {
-        "id": 31,
-        "name": "Adventure"
-      }
-    ],
-    "involved_companies": [
-      {
-        "id": 7,
-        "company": {
-          "id": 4,
-          "name": "Eidos Interactive"
-        }
-      },
-      {
-        "id": 6,
-        "company": {
-          "id": 3,
-          "name": "Looking Glass Studios"
-        }
-      },
-      {
-        "id": 265431,
-        "company": {
-          "id": 1488,
-          "name": "Activision Value"
-        }
-      }
-    ],
-    "name": "Thief: The Dark Project",
-    "platforms": [
-      {
-        "id": 6,
-        "name": "PC (Microsoft Windows)"
-      }
-    ],
-    "rating": 86.63396922169207
-  }
-    return data
-}
+    
